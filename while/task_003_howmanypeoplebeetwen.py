2abda2ad16260ef88efa415ee3201f73be690847
siexzue <sasha.alexandrin@gmail.com>
2026-05-31 07:00:34 +0300

    task peoplebtwn

---
+++ while/task_003_howmanypeoplebeetwen.py
@@ -0,0 +1,15 @@
+pos_alex = -1
+pos_lev = -1
+current_position = 0
+
+while True:
+    name = input()
+    if name == "Александра" and pos_alex == -1:
+        pos_alex = current_position
+    if name == "Левон" and pos_lev == -1:
+        pos_lev = current_position
+    if pos_alex != -1 and pos_lev != -1:
+        break
+    current_position += 1
+
+print(abs(pos_alex - pos_lev) - 1)
